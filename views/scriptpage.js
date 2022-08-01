da4b24712eabfd6b5c8af038c7924096fb2540fe
ranvir <ranveerbhatti2106@gmail.com>
2022-08-01 22:03:02 +0530

    modified

---
+++ views/scriptpage.js
@@ -0,0 +1,16 @@
+let reda = document.getElementById("black");
+let btn = document.getElementById("btn");
+let avail = document.querySelectorAll(".rota");
+
+// reda.style.background-color = "yellow"
+
+reda.addEventListener("click",function(){
+    
+})
+function fun(){
+    // reda.style.display = "block"
+    // avail.forEach()
+    avail.forEach(x => x.classList.toggle('black'));
+    // avail.classList.toggle("black")
+
+}
